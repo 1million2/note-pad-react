a5968d1b3e56110d25bb0bdf5d41fa5cb2f18771
Eduard <makarichak1209@gmail.com>
2021-09-08 12:02:38 +0300

    first

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="content">
         <p>empty list</p>
         <ul>
-          <li>Task 1</li>
+          <li>Task 2</li>
           <li>Task 1</li>
           <li>Task 1</li>
           <li>Task 1</li>
