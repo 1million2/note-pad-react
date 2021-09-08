81d51c043559f06a8884ddf80e424225ba9e2681
Eduard <makarichak1209@gmail.com>
2021-09-08 14:33:49 +0300

    add component, module style

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+function Content() {
+    return(
+      <div>
+        <h1>hello</h1>
+        <p>Content</p>
+      </div>
+
+    );
+  }
+
+  export default Content;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import classes from "./Header.module.css";
+function Header() {
+    return(
+      <p className={classes.header}>Header</p>
+    );
+  }
+
+  export default Header;
